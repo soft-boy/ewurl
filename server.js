cd0e25a3d604004173ceb8b72350ea90c7164b0d
Eric Wang <ewang1997@gmail.com>
2025-09-06 20:24:47 -0700

    koa boilerplate

---
+++ server.js
@@ -0,0 +1,25 @@
+const Koa = require('koa');
+const Router = require('koa-router');
+const bodyParser = require('koa-bodyparser');
+
+const app = new Koa();
+const router = new Router({ prefix: '/api/v1' });
+
+router.post('/data/shorten', async ctx => {
+  ctx.body = { message: 'Shorten endpoint hit' };
+});
+
+router.get('/shortUrl', async ctx => {
+  ctx.body = { message: 'ShortUrl endpoint hit' };
+});
+
+app.use(bodyParser());
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+app.use(async ctx => {
+  ctx.body = { status: 'ok' };
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port);
