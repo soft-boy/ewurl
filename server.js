fffa62136bf0e522b46d29170c29fd8c1f449b95
Eric Wang <ewang1997@gmail.com>
2025-09-07 17:08:42 -0700

    add pg connection, implement shortUrl redirects

--- server.js
+++ server.js
@@ -3,9 +3,17 @@ require('dotenv').config();
 const Koa = require('koa');
 const Router = require('@koa/router');
 const { bodyParser } = require("@koa/bodyparser");
+const { Client } = require('pg');
 
 const app = new Koa();
 const router = new Router();
+const pg = new Client({
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pg.connect()
 
 // parameters: longUrl
 // returns: { shortUrl }
@@ -14,8 +22,25 @@ router.post('/api/shorten', async ctx => {
 });
 
 // redirects to longUrl
-router.get('/shortUrl', async ctx => {
-  ctx.body = { message: 'ShortUrl endpoint hit' };
+router.get('/:shortUrl', async ctx => {
+  const shortUrl = ctx.params.shortUrl;
+  
+  try {
+    const res = await pg.query('SELECT longurl FROM urls WHERE shortUrl = $1::text', [shortUrl])
+
+    if (res.rows.length === 0) {
+      ctx.status = 404;
+      ctx.body = { error: '404 Not Found' };
+    }
+    else {
+      ctx.status = 302; // temporary redirect
+      ctx.redirect(res.rows[0].longurl);
+    }
+  } catch (err) {
+    console.error(err);
+    ctx.status = 500;
+    ctx.body = { error: 'Internal Server Error' };
+  }
 });
 
 router.get('/api/health', async ctx => {
